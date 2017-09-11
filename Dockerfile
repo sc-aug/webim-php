13e17e2f01a80b4af1a5ebaf85d2a36969a6ef9e
sc-aug <shichuanwang@yahoo.com>
2017-09-11 06:36:59 -0500

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM koolob/swoole-docker
 EXPOSE 9502
-#COPY src /root/app
-#WORKDIR /root/app
-#CMD sh /root/app/start.sh
+COPY server /root/app
+WORKDIR /root/app
+CMD sh /root/app/start.sh
--- deploy_build.sh
+++ deploy_build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # imageName="koolob/swoole-docker"
-# docker pull $imageName
+docker pull $imageName
 localImageName="swoole-server:0.1"
 containerName="swoole-server"
 docker build -t $localImageName .
@@ -8,7 +8,3 @@ docker stop $containerName
 docker rm $containerName
 docker run --name $containerName -d -p 9502:9502 $localImageName
 docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
-
-# docker pull koolob/swoole-docker
-# docker build -t swoole-server:0.1 .
-# docker run -t -i -p 8080:8080 swoole-server:0.1
